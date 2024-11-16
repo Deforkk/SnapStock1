797446ed24d7eb4add067ff84187af8741d34017
tatallia <tatallia.nn@gmail.com>
2024-11-16 21:39:46 +0100

    добавила Color и два цвета в тему, их можно напрямую вставлять (LavenderMist и JetBlack). Не уверенна насколько они сочетаются, но в теории нормально. удалила лишний код в MainActivity

--- app/src/main/java/com/example/snapstock1/ui/theme/Theme.kt
+++ app/src/main/java/com/example/snapstock1/ui/theme/Theme.kt
@@ -1,9 +1,21 @@
 package com.example.snapstock1.ui.theme
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
+private val DarkColorScheme = darkColorScheme(
+    primary = LavenderMist,
+    secondary = MauvePink,
+    tertiary = JetBlack
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = LavenderMist,
+    secondary = MauvePink,
+    tertiary = JetBlack
+)
 // Определение цветовой схемы
 private val LightColors = lightColorScheme(
     primary = androidx.compose.ui.graphics.Color(0xFF6200EE),
